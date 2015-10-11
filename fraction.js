02fca3e20dd42d3e2e72f4af28806cc3c44a603b
Jota <jalava85@gmail.com>
2015-10-11 14:16:27 -0400

    Initial tests with the js client.

--- fraction.js
+++ fraction.js
@@ -2,7 +2,7 @@ var XFoo = document.registerElement('j-cms');
 var elem = document.getElementById("test");
 function getScripts(e) {
     var r = new XMLHttpRequest();
-    r.open("GET", "http://localhost:3000/api/5619dab3ada92090225698bf", true);
+    r.open("GET", "http://localhost:3000/api/" + elem.getAttribute('data-id'), true);
     r.onreadystatechange = function () {
         if (r.readyState != 4 || r.status != 200) return;
         elem.innerText = r.responseText;
