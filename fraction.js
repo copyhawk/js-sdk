51f323a64d646104c0dbd67b7f33fc34e08cfea9
Jota <jalava85@gmail.com>
2015-10-11 14:43:57 -0400

    Rudimentary demo

--- fraction.js
+++ fraction.js
@@ -1,13 +1,17 @@
 var XFoo = document.registerElement('j-cms');
 var elem = document.getElementById("test");
+var elem2 = document.getElementById("test2");
+
+// NAIVE POC - Will have to get fancy up in here
 function getScripts(e) {
     var r = new XMLHttpRequest();
-    r.open("GET", "http://localhost:3000/api/" + elem.getAttribute('data-id'), true);
+    r.open("GET", "http://localhost:3000/api/" + e.getAttribute('data-id'), true);
     r.onreadystatechange = function () {
         if (r.readyState != 4 || r.status != 200) return;
-        elem.innerText = r.responseText;
+        e.innerText = r.responseText;
     };
     r.send("a=1&b=2&c=3")
 }
 
-getScripts();
+getScripts(elem);
+getScripts(elem2);
